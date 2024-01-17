4a44ad69b51f10b661b2afa72d3071a138c8e1a7
JaminChen0 <83471007+JaminChen0@users.noreply.github.com>
2024-01-17 05:20:42 -0500

    Update main.c

--- Dynamic Memory Allocator/src/main.c
+++ Dynamic Memory Allocator/src/main.c
@@ -2,24 +2,6 @@
 #include "sfmm.h"
 
 int main(int argc, char const *argv[]) {
-    /*double* ptr = sf_malloc(sizeof(double));
-
-    *ptr = 320320320e-320;
-
-    printf("%f\n", *ptr);
-
-    sf_free(ptr);
-
-    return EXIT_SUCCESS;*/
-    /*double* ptr = sf_malloc(sizeof(double));
-    //sf_show_heap();
-
-    *ptr = 3;
-
-    printf("%f\n", *ptr);
-
-    sf_free(ptr);*/
-
 
     sf_errno = 0;
     // We want to allocate up to exactly four pages.
